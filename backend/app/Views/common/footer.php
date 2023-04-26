d14f7a7d007261098608e9f6fdeae1d4290088f6
stoswebsolutions <stoswebsolutions@gmail.com>
2023-04-26 12:27:43 +0530

    header footer design change

--- backend/app/Views/common/footer.php
+++ backend/app/Views/common/footer.php
@@ -1,4 +1,3 @@
 <footer class="footer text-center">
-    All Rights Reserved by VRS. Designed and Developed by
-    <a href="https://vitasoft.in/">VITA TECH SOLUTIONS PVT LTD</a>.
+    All Rights Reserved by Village Survey.
 </footer>
